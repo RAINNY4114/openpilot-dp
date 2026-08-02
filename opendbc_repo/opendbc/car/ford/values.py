7ca34e47c50a1f64ed702c412d6dc186ac4ba967
RAINNY4114 <152569703+RAINNY4114@users.noreply.github.com>
2026-08-02 22:30:48 +0800

    Update values.py

--- opendbc_repo/opendbc/car/ford/values.py
+++ opendbc_repo/opendbc/car/ford/values.py
@@ -126,7 +126,7 @@ class CAR(Platforms):
     CarSpecs(mass=1625, wheelbase=2.67, steerRatio=17.7),
   )
   FORD_EDGE_MK2 = FordPlatformConfig(
-    [FordCarDocs("Ford Edge 2022")
+    [FordCarDocs("Ford Edge 2022"), 
      FordCarDocs("Lincoln Nautilus 2018-21", "Adaptive Cruise Control with Lane Centering")],    
     CarSpecs(mass=2050, wheelbase=3.025, steerRatio=19.33),
     flags=FordFlags.ALT_STEER_ANGLE,
