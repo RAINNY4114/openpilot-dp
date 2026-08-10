4e4b6033407fcb26dcae00b2f4516d55d8b2147a
zhuory <107243601+zhuory@users.noreply.github.com>
2026-08-10 16:47:21 +0800

    Update values.py

--- opendbc_repo/opendbc/car/ford/values.py
+++ opendbc_repo/opendbc/car/ford/values.py
@@ -181,7 +181,8 @@ class CAR(Platforms):
   )
   LINCOLN_NAUTILUS_MKX = FordPlatformConfig(
     [FordCarDocs("Lincoln Nautilus 2018-21", "Adaptive Cruise Control with Lane Centering")],
-    CarSpecs(mass=2050, wheelbase=3.025, steerRatio=19.3),    
+    CarSpecs(mass=2050, wheelbase=3.025, steerRatio=19.3),
+    flags=FordFlags.ALT_STEER_ANGLE,
   )
 
 
